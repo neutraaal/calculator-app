d284ed1abe5516b48b0403956e1aa77dc1d89087
neutraaal <tripeace07@gmail.com>
2025-04-06 18:50:46 +0900

    placing component parts

---
+++ calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  imports: [],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.scss'
+})
+export class CalculatorComponent {
+
+  result: number;
+
+}
